666644221bbb7a3cf9b2df241ca3c6498df52234
gypseee <64298049+gypseee@users.noreply.github.com>
2020-04-25 11:45:31 +0545

    Add files via upload

---
+++ src/header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+function Header() {
+return(
+<header role="banner">
+  <div id="inner-header" class="clearfix">
+    <div class="navbar navbar-default top-bg">
+      <div class="container" id="navbarcont">
+        <div class="row">
+        <div class="nav-container col-md-9">
+          <nav role="navigation">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+              <a href="http://202.51.75.82/wordpress/" class="navbar-brand logo" rel="home"><img original-width="211" original-height="51" src="http://202.51.75.82/wordpress/wp-content/uploads/2020/03/cropped-logo.png" class="custom-logo" alt="Datahub Wiki" srcset="http://202.51.75.82/wordpress/wp-content/uploads/2020/03/cropped-logo.png 211w, http://202.51.75.82/wordpress/wp-content/uploads/2020/03/cropped-logo-150x36.png 150w" sizes="(max-width: 211px) 100vw, 211px"/></a>            </div>
+
+          </nav>
+        </div>
+        <div class="navbar-collapse collapse top-menu">
+          <div class="menu-navigation-container"><ul id="menu" class="nav navbar-nav navbar-right mywiki-header-menu"><li id="menu-item-80" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-80"><a href="http://202.51.75.82/wordpress/index.php/category/it/">IT</a>
+<ul class="sub-menu">
+	<li id="menu-item-104" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-104"><a href="http://202.51.75.82/wordpress/index.php/category/it/cloud/">Cloud</a>
+	<ul class="sub-menu">
+		<li id="menu-item-132" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-132"><a href="http://202.51.75.82/wordpress/index.php/category/it/cloud/installation-and-configuration-cloud/">Installation And Configuration</a></li>
+		<li id="menu-item-133" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-133"><a href="http://202.51.75.82/wordpress/index.php/category/it/cloud/problem-and-solutions/">Problem and Solutions</a></li>
+	</ul>
+</li>
+	<li id="menu-item-103" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-103"><a href="http://202.51.75.82/wordpress/index.php/category/it/backup-and-replication/">BackUp and Replication</a>
+	<ul class="sub-menu">
+		<li id="menu-item-128" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-128"><a href="http://202.51.75.82/wordpress/index.php/category/it/backup-and-replication/installation-backup-and-replication/">Installation And Configuration</a></li>
+		<li id="menu-item-129" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-129"><a href="http://202.51.75.82/wordpress/index.php/category/it/backup-and-replication/problem-and-solution/">Problem and solution</a></li>
+	</ul>
+</li>
+	<li id="menu-item-85" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-85"><a href="http://202.51.75.82/wordpress/index.php/category/it/network/">Network</a>
+	<ul class="sub-menu">
+		<li id="menu-item-86" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-86"><a href="http://202.51.75.82/wordpress/index.php/category/it/network/installation-and-configuration/">Installation and Configuration</a></li>
+		<li id="menu-item-92" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-92"><a href="http://202.51.75.82/wordpress/index.php/category/it/network/problems-network/">Problems</a></li>
+	</ul>
+</li>
+	<li id="menu-item-88" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-88"><a href="http://202.51.75.82/wordpress/index.php/category/it/system/">System</a>
+	<ul class="sub-menu">
+		<li id="menu-item-89" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-89"><a href="http://202.51.75.82/wordpress/index.php/category/it/system/installation/">Installation</a></li>
+		<li id="menu-item-94" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-94"><a href="http://202.51.75.82/wordpress/index.php/category/it/system/problems/">Problems</a></li>
+	</ul>
+</li>
+</ul>
+</li>
+<li id="menu-item-81" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-81"><a href="http://202.51.75.82/wordpress/index.php/category/electrical/">Electrical</a></li>
+<li id="menu-item-82" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-82"><a href="http://202.51.75.82/wordpress/index.php/category/sales-and-marketing/">Sales and Marketing</a></li>
+<li id="menu-item-114" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-114"><a href="http://202.51.75.82/wordpress/index.php/it/admin/">Admin Department</a></li>
+</ul></div>        </div>
+
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  
+</header>
+)
+}
+
+export default Header;
